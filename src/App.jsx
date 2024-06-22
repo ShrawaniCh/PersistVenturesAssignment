257c3ded0a15f2655653a1a0b642e19e22cf49d1
ShrawaniCh <shrawanibc@gmail.com>
2024-06-22 16:04:52 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
    
       <Routes>
-        <Route path='/PersistVenturesAssignment' element={<NewsListing/>}/>
+        <Route path='/' element={<NewsListing/>}/>
         <Route path='/news-details/:news_id' element={<NewsDetails/>}/>
 
 
